2e30de82e6d8aa44b063094868ff472f462a7604
Dan Drumwright <danieldrumwright@gmail.com>
2017-11-14 14:29:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 // write your code below!
 
 var name = "Daniel"
-var height = "71"
+var height = "74"
 
 console.log("Name:")
 console.log(name)
 
 console.log("Height:")
-console.log(height)
+console.log(height+1)
 
  
